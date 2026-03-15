09fc730ca09d108f22ae8ef7463fcd0edbeea0da
mhussein808 <mhussein666666@gmail.com>
2026-03-15 22:44:34 +0200

    updating files

--- hospital_module/models/__init__.py
+++ hospital_module/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from . import patient
-from . import doctor
-from . import appointment
+from . import patient      # noqa: F401
+from . import doctor       # noqa: F401
+from . import appointment  # noqa: F401
